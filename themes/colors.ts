f94616ec94dbdd5711322d4138918713f0333bb8
PedroHenr1que1 <pedrohenriquefaria9@gmail.com>
2023-01-09 20:49:14 -0300

    feat: :sparkles: Add theme colors

---
+++ themes/colors.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+  primary: '#30157F',
+  secundary: '#EF4223',
+
+  black: '#000000',
+
+  backgroundColorPrimary: '#000000',
+  backgroundColorSecondary: '#30157FA8',
+
+  succes: '#117C00',
+  succesShadow: '#117C001A',
+
+  alert: '#F2C94C',
+  alertShadow: '#F2C94C1A',
+
+  error: '#C50808',
+  errorShadow: '#C508081A',
+};
