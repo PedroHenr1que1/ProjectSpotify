41a0e91c8d8d83540974a696b0ac3a3bb8f01bc2
PedroHenr1que1 <pedrohenriquefaria9@gmail.com>
2023-01-09 22:42:25 -0300

    feat: :sparkles: Add login page

--- pages/Login/login.styles.ts
+++ pages/Login/login.styles.ts
@@ -1,9 +1,58 @@
 import styled from "styled-components";
+import Image from 'next/image';
 
 import { COLORS } from "../../themes/colors";
 
 export const Background = styled.div`
+  display: flex;
   width: 100vw;
   height: 100vh;
   background: linear-gradient(to bottom , ${COLORS.backgroundColorPrimary} 60%,${COLORS.backgroundColorSecondary});
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0px;
+`;
+
+export const ContainerInputs= styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Description = styled.p`
+  width: 18vw;
+  color: ${COLORS.fontSecondary};
+  font-size: 12px;
+  text-align: center
+`;
+
+export const Logo = styled(Image)`
+  margin: 40px 0px 20px 0px;
+`;
+
+export const Wrapper = styled.div`
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #FFFFFF33;
+  border-radius: 16px;
+  box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+`;
+
+export const Warning = styled.p`
+  color: ${COLORS.fontSecondary};
+  font-size: 12px;
 `;
--- pages/Login/login.tsx
+++ pages/Login/login.tsx
@@ -1,6 +1,14 @@
 import {
-  Background
+  Background,
+  ContainerButton,
+  ContainerInputs,
+  Description,
+  Logo as SpotifyLogo,
+  Wrapper,
+  Warning,
 } from './login.styles'
+
+import Logo from '../../assets/Svg/SpotifyLogo.svg';
 
 import TextInput from '../../components/common/TextInput/TextInput';
 import TextInputPassword from '../../components/common/TextInputPassword/TextInputPassword';
@@ -9,18 +17,29 @@ import Button from '../../components/common/Button/Button';
 export default function Login() {
   return (
     <Background>
-      <TextInput
-        title='Email'
-        placeholder='Informe seu email'
-      />
-      <TextInputPassword
-        title='Senha'
-        placeholder='Informe sua senha'
-        securityPassword={true}
-      />
-      <Button
-        title='Entrar'
-      />
+      <Wrapper>
+        <SpotifyLogo width={250} draggable={false} src={Logo} alt="Logo" />
+        <Description>
+          Faça login com sua conta Spotify e tenha acesso a diversas informações sobre seus artistas preferidos e seu gosto musical!
+        </Description>
+        <ContainerInputs>
+          <TextInput
+            title='Email'
+            placeholder='Informe seu email'
+          />
+          <TextInputPassword
+            title='Senha'
+            placeholder='Informe sua senha'
+            securityPassword={true}
+          />
+        </ContainerInputs>
+        <ContainerButton>
+          <Button
+            title='Entrar'
+          />
+          <Warning>Nenhum dado é utilizado sem sua autorização!</Warning>
+        </ContainerButton>
+      </Wrapper>
     </Background>
   )
 }
