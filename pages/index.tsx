71c05e6253a47eafc909e079ad3d9d3b2e8a9502
PedroHenr1que1 <pedrohenriquefaria9@gmail.com>
2023-01-09 23:26:56 -0300

    refactor: :art: Improve code structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,45 @@
-import Login from "./Login/index.pages"
+import {
+  Background,
+  ContainerButton,
+  ContainerInputs,
+  Description,
+  Logo as SpotifyLogo,
+  Wrapper,
+  Warning,
+} from './Login/login.styles'
 
-export default function Routes() {
+import Logo from '../../assets/Svg/SpotifyLogo.svg';
+
+import TextInput from '../components/common/TextInput/TextInput';
+import TextInputPassword from '../components/common/TextInputPassword/TextInputPassword';
+import Button from '../components/common/Button/Button';
+
+export default function Login() {
   return (
-    <>
-      <Login/>
-    </>
+    <Background>
+      <Wrapper>
+        <SpotifyLogo width={250} draggable={false} src={Logo} alt="Logo" />
+        <Description>
+          Faça login com sua conta Spotify e tenha acesso a diversas informações sobre seus artistas preferidos e seu gosto musical!
+        </Description>
+        <ContainerInputs>
+          <TextInput
+            title='Email'
+            placeholder='Informe seu email'
+          />
+          <TextInputPassword
+            title='Senha'
+            placeholder='Informe sua senha'
+            securityPassword={true}
+          />
+        </ContainerInputs>
+        <ContainerButton>
+          <Button
+            title='Entrar'
+          />
+          <Warning>Nenhum dado é utilizado sem sua autorização!</Warning>
+        </ContainerButton>
+      </Wrapper>
+    </Background>
   )
 }
